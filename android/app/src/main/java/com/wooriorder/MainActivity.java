6abf7254dc97771615c31835bbbfd484b49d95bc
munkyuhwan <55966981+munkyuhwan@users.noreply.github.com>
2023-08-11 22:58:59 +0900

    화면 켜짐 유지

--- android/app/src/main/java/com/wooriorder/MainActivity.java
+++ android/app/src/main/java/com/wooriorder/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // 화면 켜짐 유지
     getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -45,6 +46,10 @@ public class MainActivity extends ReactActivity {
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+    WindowManager.LayoutParams params = getWindow().getAttributes();
+    params.screenBrightness= 0.1F;
+    getWindow().setAttributes(params);
 
   }
 }
