f051c2ee1751caa32db88187892b242b8ba9b54e
munkyuhwan <55966981+munkyuhwan@users.noreply.github.com>
2023-11-18 00:41:56 +0900

    카테고리 언어 수정

--- src/components/menuComponents/topMenuList.js
+++ src/components/menuComponents/topMenuList.js
@@ -16,6 +16,44 @@ const TopMenuList = (props) => {
     const {selectedMainCategory, selectedSubCategory, subCategories} = useSelector((state)=>state.categories);
     const [selectedCode, setSelectedCode] = useState(DEFAULT_CATEGORY_ALL_CODE);
 
+    const {menuCategories} = useSelector(state=>state.menuExtra);
+    const {language} =  useSelector(state=>state.languages);
+    
+    const ItemTitle = (cateCode) => {
+        const subCategories = menuCategories?.filter(el=>el.cate_code1 == selectedMainCategory);
+        const subCatList = subCategories[0].level2
+        const subCatData = subCatList?.filter(el=>el.cate_code2 == cateCode);
+        let selTitleLanguage = "";
+        if(language=="korean") {
+            selTitleLanguage = subCatData[0].cate_name2
+        }
+        else if(language=="japanese") {
+            selTitleLanguage = subCatData[0].cate_name2_jp
+        }
+        else if(language=="chinese") {
+            selTitleLanguage = subCatData[0].cate_name2_cn
+        }
+        else if(language=="english") {
+            selTitleLanguage = subCatData[0].cate_name2_en
+        }
+        return selTitleLanguage;
+    }
+    const ItemWhole = () =>{
+        let selTitleLanguage = "";
+        if(language=="korean") {
+            selTitleLanguage = '전체'
+        }
+        else if(language=="japanese") {
+            selTitleLanguage = "全体"
+        }
+        else if(language=="chinese") {
+            selTitleLanguage = "全部的"
+        }
+        else if(language=="english") {
+            selTitleLanguage = "ALL"
+        }
+        return selTitleLanguage; 
+    }
     useEffect(()=>{
         if(selectedCode!=null) {
             props?.onSelectItem(selectedCode);
@@ -31,14 +69,14 @@ const TopMenuList = (props) => {
         {selectedSubCategory == DEFAULT_CATEGORY_ALL_CODE &&
             <TouchableWithoutFeedback key={"subcat_"} onPress={()=>{}}>
                 <CategorySelected>
-                    <TopMenuText key={"subcatText_"} >전체</TopMenuText>
+                    <TopMenuText key={"subcatText_"} >{ItemWhole()}</TopMenuText>
                 </CategorySelected>
             </TouchableWithoutFeedback>
         }
         {selectedSubCategory != DEFAULT_CATEGORY_ALL_CODE &&
             <TouchableWithoutFeedback key={"subcat_"} onPress={()=>{onPressAction(DEFAULT_CATEGORY_ALL_CODE);}}>
                 <CategoryDefault>
-                <TopMenuText key={"subcatText_"} >전체</TopMenuText>
+                <TopMenuText key={"subcatText_"} >{ItemWhole()}</TopMenuText>
                 </CategoryDefault>
             </TouchableWithoutFeedback>
         }
@@ -50,7 +88,7 @@ const TopMenuList = (props) => {
                         (el?.cate_code2==selectedSubCategory) &&
                             <TouchableWithoutFeedback key={"subcat_"+el?.cate_code2} onPress={()=>{ onPressAction(el?.cate_code2); }}>
                                 <CategorySelected>
-                                    <TopMenuText key={"subcatText_"+el?.cate_code2} >{el.cate_name2}</TopMenuText>
+                                    <TopMenuText key={"subcatText_"+el?.cate_code2} >{ItemTitle(el?.cate_code2)}</TopMenuText>
                                 </CategorySelected>
                             </TouchableWithoutFeedback>
                         }
@@ -58,7 +96,7 @@ const TopMenuList = (props) => {
                         (el?.cate_code2!=selectedSubCategory) &&
                             <TouchableWithoutFeedback key={"subcat_"+el?.cate_code2} onPress={()=>{ onPressAction(el?.cate_code2); }}>
                                 <CategoryDefault>
-                                    <TopMenuText key={"subcatText_"+el?.cate_code2} >{el.cate_name2}</TopMenuText>
+                                    <TopMenuText key={"subcatText_"+el?.cate_code2} >{ItemTitle(el?.cate_code2)}</TopMenuText>
                                 </CategoryDefault>
                             </TouchableWithoutFeedback>
                         }
